7a371514c5560317f6ea021289b2f44960664f23
q89771441 <53997668+q89771441@users.noreply.github.com>
2022-10-18 12:59:22 +0800

    update: test

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import time
+
+url = "https://api.etherscan.io/api"
+
+payload={"module": "account",
+         "action": "txlist",
+         "address": "0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC",
+         "startblock": 15000000,
+         "endblock": 15000099,
+         "page": 1,
+         "sort": "desc",
+         "apikey": ""}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+result = json.loads(response.text)['result']
+target = 16000000
+store = []
+cnt = 0
+while(payload["startblock"] <= target):
+    response = requests.request("GET", url, headers=headers, data=payload)
+    result = json.loads(response.text)['result']
+    store += result
+    payload["startblock"] = payload["startblock"] + 100
+    payload["endblock"] =  payload["startblock"] + 99
+    cnt += 1
+    if(cnt % 5 == 0):
+        time.sleep(1)
+    print(len(store))
+
+print(len(store))
