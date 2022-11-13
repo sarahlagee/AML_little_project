96eb068ca5d7d24e822763845490ed158a90a573
sarahlagee <116254901+sarahlagee@users.noreply.github.com>
2022-11-13 23:01:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ payload={"module": "account",
          "page": 1,
          "sort": "desc",
          "apikey": ""}
-headers = {}
+headers = {
+         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+}
 
 response = requests.request("GET", url, headers=headers, data=payload)
 
